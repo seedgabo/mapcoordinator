78ae7f029b2595331cec95bf50a9d2fe7084f99c
gabriel <seedgabo@gmail.com>
2018-07-17 13:58:03 -0500

    place user selector

---
+++ src/components/PlaceUserSelector.vue
@@ -0,0 +1,79 @@
+<template>
+  <v-card>
+    <v-card-title class="headline" primary-title>
+      Selector
+    </v-card-title>
+    <v-card-text>
+      <v-layout>
+
+        <v-flex sm6>
+          <v-text-field solo v-model="query_places"></v-text-field>
+          <v-list>
+            <v-list-tile v-for="place in filterBy(places,query_places)" :key="place.id">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ place.name }}</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-checkbox v-model="place._selected"></v-checkbox>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+        </v-flex>
+
+        <v-flex sm6>
+          <v-text-field solo v-model="query_users"></v-text-field>
+          <v-list>
+            <v-list-tile v-for="user in filterBy(users,query_users)" :key="user.id">
+              <v-list-tile-content>
+                <v-list-tile-title>{{ user.name }}</v-list-tile-title>
+              </v-list-tile-content>
+              <v-list-tile-action>
+                <v-checkbox v-model="user._selected"></v-checkbox>
+              </v-list-tile-action>
+            </v-list-tile>
+          </v-list>
+        </v-flex>
+
+      </v-layout>
+    </v-card-text>
+    <v-card-actions>
+      <v-spacer></v-spacer>
+      <v-btn flat @click="$emit('cancel')">Cancel</v-btn>
+      <v-btn flat @click="ready()" color="primary">Ok</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+<script>
+export default {
+	name: "PlaceUserSelector",
+	mounted() {
+		this.users = this.getusers();
+		this.places = this.getplaces();
+	},
+	methods: {
+		ready() {
+			var data = {
+				users: this.users.filter((u) => {
+					return u._selected;
+				}),
+				places: this.places.filter((p) => {
+					return p._selected;
+				})
+			};
+
+			this.$emit("reaady", data);
+		}
+	},
+	data() {
+		return {
+			users: [],
+			seleced_users: [],
+			query_users: "",
+
+			places: [],
+			seleced_places: [],
+			query_places: ""
+		};
+	}
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,43 +1,41 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import App from './App'
-import router from './router'
-import Vuetify from 'vuetify'
-import 'vuetify/dist/vuetify.min.css'
-import storage from './mixins/storage.js';
-Vue.use(Vuetify)
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App";
+import router from "./router";
+import Vuetify from "vuetify";
+import Vue2Filters from "vue2-filters";
+import "vuetify/dist/vuetify.min.css";
+import storage from "./mixins/storage.js";
 
+Vue.use(Vuetify);
+Vue.use(Vue2Filters);
+Vue.config.productionTip = false;
 
 // Mixins
 Vue.mixin(storage);
 
-
 // Filters
-Vue.filter('distance', function (value) {
-  if (!value) return ''
-  value = parseFloat(value.toString())
+Vue.filter("distance", function (value) {
+  if (!value) return "";
+  value = parseFloat(value.toString());
   if (value > 1000) {
     return (value / 1000).toFixed(1) + "Km";
   }
-  return (value).toFixed(1) + "mts";
-})
-
+  return value.toFixed(1) + "mts";
+});
 
 // Components
-Vue.component('map-user-dialog', require("./components/dialogs/UserDialog.vue").default);
-Vue.component('map-place-dialog', require("./components/dialogs/PlaceDialog.vue").default);
-Vue.component('nearby-places-dialog', require("./components/dialogs/NearbyPlaces.vue").default);
-
-
-
+Vue.component("map-user-dialog", require("./components/dialogs/UserDialog.vue").default);
+Vue.component("map-place-dialog", require("./components/dialogs/PlaceDialog.vue").default);
+Vue.component("nearby-places-dialog", require("./components/dialogs/NearbyPlaces.vue").default);
+Vue.component("place-user-selector", require("./components/PlaceUserSelector.vue").default);
 
 new Vue({
-  el: '#app',
+  el: "#app",
   router,
   components: {
     App
   },
-  template: '<App/>'
-})
+  template: "<App/>"
+});
